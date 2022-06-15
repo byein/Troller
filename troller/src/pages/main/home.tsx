5a93a944a3768f00009b03c4e78d083b2781ae4f
byein <kyebin0825@gmail.com>
2022-06-16 00:15:49 +0900

    feat: create Home page - close #5, #6, #7

--- troller/src/pages/main/home.tsx
+++ troller/src/pages/main/home.tsx
@@ -1,4 +1,18 @@
+import { GeneralRank, ProRank } from "../../components/home/rank";
+import Search from "../../components/home/search";
+import Title from "../../components/home/title";
+import { HomeWrapper, Section } from "../../styles/home/home";
+
 function Home() {
-	return null;
+  return (
+    <HomeWrapper>
+      <Title></Title>
+      <Search></Search>
+      <Section>
+        <GeneralRank></GeneralRank>
+        <ProRank></ProRank>
+      </Section>
+    </HomeWrapper>
+  );
 }
 export default Home;
