aa1c330d5c28cfd460161592906f2cde8a6c1877
CWCTBOY <qkrdlswo98@gmail.com>
2022-06-17 22:00:56 +0900

    feat: react-hook-form으로 로그인 input값 저장기능까지 구현 close #8

--- troller/src/pages/sign/signin.tsx
+++ troller/src/pages/sign/signin.tsx
@@ -1,28 +1,44 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import AnotherLogin from '../../components/sign/anotherLogin';
-import SignForm from '../../components/sign/signForm';
+import SignForm, { ISignType } from '../../components/sign/signForm';
 import { Form, InputBox, SubmitBtn } from '../../styles/sign/globalSignBox';
 
 function Signin() {
+	const { register, handleSubmit } = useForm<ISignType>();
+	const onSubmit = (data: ISignType) => {
+		console.log(data);
+		// data에 email && password값 저장 => 서버 전송코드 작성구간
+	};
 	return (
 		<SignForm>
-			<Form>
+			<Form onSubmit={handleSubmit(onSubmit)}>
 				<InputBox>
 					<div className="label">
 						<span className="label__name">Email address</span>
 					</div>
-					<input type="email" />
+					<input
+						{...register('email', {
+							required: true,
+						})}
+						type="email"
+						placeholder="Logo@example.com"
+					/>
 				</InputBox>
 				<InputBox>
 					<div className="label">
 						<span className="label__name">Password</span>
 						<Link to="/forgot_pw">
-							<span className="label__forgotpw">forgot password</span>
+							<span className="label__forgotpw">forgot password?</span>
 						</Link>
 					</div>
-					<input type="text" />
+					<input
+						{...register('password', {
+							required: true,
+						})}
+					/>
 				</InputBox>
-				<SubmitBtn>Log In</SubmitBtn>
+				<SubmitBtn type="submit">Log In</SubmitBtn>
 			</Form>
 			<Link to="/sign_up">
 				<SubmitBtn>Create Account</SubmitBtn>
