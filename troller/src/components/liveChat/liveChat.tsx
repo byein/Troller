800664f6541c1836a491b4cab39f8c48e1916497
CWCTBOY <qkrdlswo98@gmail.com>
2022-08-27 13:16:11 +0900

    test

--- troller/src/components/liveChat/liveChat.tsx
+++ troller/src/components/liveChat/liveChat.tsx
@@ -12,24 +12,28 @@ function LiveChat() {
     chatRoomId: string;
   }>();
 
-  const client = new StompJs.Client({
+  const client1 = new StompJs.Client({
     brokerURL: 'ws://3.37.22.89:8080/ws-chat',
     reconnectDelay: 5000,
-    onConnect: frame => {
-      console.log(`Connected: ${frame}`);
-      client.subscribe(`ws://topic/chat_room/${chatRoomId}`, message => {
-        console.log(message);
-      });
-    },
-    onStompError: frame => {
-      console.log(`Broker reported error: ${frame.headers.message}`);
-      console.log(`Additional details: ${frame.body}`);
-    },
   });
 
   useEffect(() => {
+    const client = new StompJs.Client({
+      brokerURL: 'ws://3.37.22.89:8080/ws-chat',
+      reconnectDelay: 5000,
+      onConnect: frame => {
+        console.log(`Connected: ${frame}`);
+        client.subscribe(`ws://topic/chat_room/${chatRoomId}`, message => {
+          console.log(message);
+        });
+      },
+      onStompError: frame => {
+        console.log(`Broker reported error: ${frame.headers.message}`);
+        console.log(`Additional details: ${frame.body}`);
+      },
+    });
     client.activate();
-  });
+  }, [chatRoomId, opponentLolName]);
 
   const onSend = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -39,7 +43,7 @@ function LiveChat() {
         content,
         accessToken,
       };
-      client.publish({
+      client1.publish({
         destination: '/app/chat/message',
         body: JSON.stringify(message),
       });
