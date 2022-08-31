896e865824428f6e386b17e92c7ae56ac3fec3f3
byein <kyebin0825@gmail.com>
2022-08-31 19:26:51 +0900

    chore: meaningless change. ignore them

--- troller/src/components/liveChat/liveChat.tsx
+++ troller/src/components/liveChat/liveChat.tsx
@@ -1,72 +1,71 @@
-import SendIcon from '@mui/icons-material/Send';
-import React, { useEffect, useState } from 'react';
-import * as StompJs from '@stomp/stompjs';
-import { useOutletContext } from 'react-router-dom';
-import { Form, TextArea } from '../../styles/liveChat/liveChat';
+import SendIcon from "@mui/icons-material/Send";
+import React, { useEffect, useState } from "react";
+import * as StompJs from "@stomp/stompjs";
+import { useOutletContext } from "react-router-dom";
+import { Form, TextArea } from "../../styles/liveChat/liveChat";
 
 function LiveChat() {
-  const [content, setContent] = useState('');
-  const accessToken = localStorage.getItem('access_token');
-  const { opponentLolName, chatRoomId } = useOutletContext<{
-    opponentLolName: string;
-    chatRoomId: string;
-  }>();
+	const [content, setContent] = useState("");
+	const accessToken = localStorage.getItem("access_token");
+	const { opponentLolName, chatRoomId } = useOutletContext<{
+		opponentLolName: string;
+		chatRoomId: string;
+	}>();
 
-  const client1 = new StompJs.Client({
-    brokerURL: 'ws://3.37.22.89:8080/ws-chat',
-    reconnectDelay: 5000,
-  });
+	const client1 = new StompJs.Client({
+		brokerURL: "ws://3.37.22.89:8080/ws-chat",
+		reconnectDelay: 5000,
+	});
 
-  useEffect(() => {
-    const client = new StompJs.Client({
-      brokerURL: 'ws://3.37.22.89:8080/ws-chat',
-      reconnectDelay: 5000,
-      onConnect: frame => {
-        console.log(`Connected: ${frame}`);
-        client.subscribe(`ws://topic/chat_room/${chatRoomId}`, message => {
-          console.log(message);
-        });
-      },
-      onStompError: frame => {
-        console.log(`Broker reported error: ${frame.headers.message}`);
-        console.log(`Additional details: ${frame.body}`);
-      },
-    });
-    client.activate();
-  }, [chatRoomId, opponentLolName]);
+	const client = new StompJs.Client({
+		brokerURL: "ws://3.37.22.89:8080/ws-chat",
+		reconnectDelay: 5000,
+		onConnect: (frame) => {
+			console.log(`Connected: ${frame}`);
+			client.subscribe(`ws://topic/chat_room/${chatRoomId}`, (message) => {
+				console.log(message);
+			});
+		},
+		onStompError: (frame) => {
+			console.log(`Broker reported error: ${frame.headers.message}`);
+			console.log(`Additional details: ${frame.body}`);
+		},
+	});
+	client.activate();
+	useEffect(() => {}, [chatRoomId, opponentLolName]);
 
-  const onSend = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (content !== '') {
-      const message = {
-        chatRoomId,
-        content,
-        accessToken,
-      };
-      client1.publish({
-        destination: '/app/chat/message',
-        body: JSON.stringify(message),
-      });
-    }
-    setContent('');
-  };
-  return (
-    <>
-      <TextArea />
-      <Form onSubmit={onSend}>
-        <input
-          className="input"
-          name="message"
-          value={content}
-          onChange={e => setContent(e.target.value)}
-          placeholder="메시지를 입력하세요"
-        />
-        <button type="button" className="send">
-          <SendIcon />
-        </button>
-      </Form>
-    </>
-  );
+	const onSend = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (content !== "") {
+			const message = {
+				chatRoomId,
+				content,
+				accessToken,
+			};
+			client1.publish({
+				destination: "/app/chat/message",
+				body: JSON.stringify(message),
+			});
+		}
+		setContent("");
+	};
+	return (
+		<>
+			<TextArea />
+			<Form onSubmit={onSend}>
+				<input
+					className="input"
+					name="message"
+					value={content}
+					onChange={(e) => setContent(e.target.value)}
+					placeholder="메시지를 입력하세요"
+				/>
+				<button type="button" className="send">
+					<SendIcon />
+				</button>
+			</Form>
+		</>
+	);
 }
 
 export default LiveChat;
